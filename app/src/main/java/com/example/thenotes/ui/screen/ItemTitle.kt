abc5a38d45dc78e56a52401067466157f46514a8
mkdzt <max.detchik@gmail.com>
2022-06-01 20:04:54 +0300

    remove import

--- app/src/main/java/com/example/thenotes/ui/screen/ItemTitle.kt
+++ app/src/main/java/com/example/thenotes/ui/screen/ItemTitle.kt
@@ -7,7 +7,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
 @Composable
