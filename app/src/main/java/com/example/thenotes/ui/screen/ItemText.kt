c392065b07de2db1a07c586c684742f046c77346
mkdzt <max.detchik@gmail.com>
2022-06-01 20:06:00 +0300

    add item text

---
+++ app/src/main/java/com/example/thenotes/ui/screen/ItemText.kt
@@ -0,0 +1,24 @@
+package com.example.thenotes.ui.screen
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ItemText(text:String,modifier: Modifier){
+    Text(
+        text = text,
+        modifier = modifier,
+        color = Color.Black,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = FontFamily.SansSerif,
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 2
+    )
+}
