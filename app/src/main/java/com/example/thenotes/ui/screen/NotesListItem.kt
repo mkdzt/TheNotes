61a702648ff59ea4b1bdf7a5f022747c4bf0fbea
mkdzt <max.detchik@gmail.com>
2022-06-01 22:22:13 +0300

    refactor and add preview

--- app/src/main/java/com/example/thenotes/ui/screen/NotesListItem.kt
+++ app/src/main/java/com/example/thenotes/ui/screen/NotesListItem.kt
@@ -1,18 +1,19 @@
 package com.example.thenotes.ui.screen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.thenotes.extensions.toDateString
 import com.example.thenotes.model.Note
+import java.util.*
 
 @Composable
 fun NotesItem(modifier: Modifier, note: Note){
@@ -20,26 +21,31 @@ fun NotesItem(modifier: Modifier, note: Note){
         modifier = modifier,
         elevation = 10.dp
     ) {
-        Row {
-            Column(
+        Column(
+            modifier = Modifier
+                .padding(start = 16.dp)
+        ) {
+            ItemBody(
+                title = note.title,
+                text = note.text
+            )
+            ItemMetaDataFunctions(
+                metadata = note.date.toDateString(),
                 modifier = Modifier
-                    .padding(horizontal = 16.dp)
-            ) {
-                Text(
-                    text = note.title,
-                    fontSize = 30.sp,
-                    fontFamily = FontFamily.SansSerif,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                )
-                Text(
-                    text = note.text,
-                    fontSize = 24.sp,
-                    fontFamily = FontFamily.SansSerif,
-                    overflow = TextOverflow.Ellipsis,
-                    maxLines = 2
-                )
-            }
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+            )
         }
     }
+}
+
+@Preview
+@Composable
+fun NotesItemPreview(){
+    NotesItem(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        note = Note(id = 1, title = "Title", text = "Text", date = Date())
+    )
 }
